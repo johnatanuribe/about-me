ebe3ceb57ef4e2bfb86d532f10a6f2b64a9edddd
Johnatan Uribe <johnatanuribe@gmail.com>
2016-07-21 01:21:07 -0700

    Fixed bug in Chrome and Firefox

--- js/index.js
+++ js/index.js
@@ -171,14 +171,14 @@ jQuery(document).ready(function($){
     }
 });
 
-
+/////MAP
 
 // svg path for target icon
 var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
 // svg path for plane icon
 var planeSVG = "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47";
 
-AmCharts.makeChart("chartdiv", {
+window.map = AmCharts.makeChart("chartdiv", {
   type: "map",
   fontSize:20,
   balloon:{horizontalPadding:20, verticalPadding:15},
@@ -252,7 +252,10 @@ AmCharts.makeChart("chartdiv", {
   linesSettings: {
     color: "#585869",
     alpha: 0.4
-  }
+  },
+    export:{
+     enabled:true
+    }
 });
 
 
@@ -501,6 +504,6 @@ switch(direction) {
 
     }
 
-});
+  });
 
 });
